dd9c9a0ef5bd7e0e5bc083f09b1de79cfd873581
yongbin0228 <bin3525@naver.com>
2019-06-01 20:53:36 +0900

    update

--- src/Study/Test2.java
+++ src/Study/Test2.java
@@ -1,7 +1,7 @@
 package Study;
 
-public class TestCode {
+public class Test2 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Test");
     }
 }
